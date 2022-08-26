9926fa9b8406054e7ebbf4d2cbaafffa69fe010b
taoufik ahjam <taoufikahjam@gmail.com>
2022-08-27 00:44:22 +0200

    add navBar and custom fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins'],
+        roboto: ['Roboto'],
+      },
+    },
   },
   plugins: [],
-}
+};
