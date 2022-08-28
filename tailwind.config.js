a204cbbe1248b5a006f3b8e9efbb7739b0c3c470
taoufik ahjam <taoufikahjam@gmail.com>
2022-08-28 17:52:46 +0200

    navBar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+    },
+    container: {
+      center: true,
+      padding: '1rem', // 16px
+    },
     extend: {
+      colors: {
+        primary: '#FD3D57',
+      },
       fontFamily: {
         poppins: ['Poppins'],
         roboto: ['Roboto'],
